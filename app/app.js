4012efa11b2dd588ef07604626b320b8752995d1
gria001 <daniel.grima@medirect.com.mt>
2023-11-12 09:44:25 +0100

    @nativescript-community/chart test

--- app/app.js
+++ app/app.js
@@ -1,7 +1,15 @@
-import Vue from 'nativescript-vue'
+import Vue from "nativescript-vue";
 
-import Home from './components/Home'
+import Home from "./components/Home";
+
+Vue.registerElement(
+  "LineChart",
+  () => require("@nativescript-community/ui-chart/charts").LineChart
+);
+
+import { install as installGestures } from "@nativescript-community/gesturehandler";
+installGestures();
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
-}).$start()
+  render: (h) => h("frame", [h(Home)]),
+}).$start();
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,127 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+  <Page>
+    <ActionBar>
+      <Label text="Home" />
+    </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+    <StackLayout>
+      <LineChart
+        height="300"
+        ref="chart"
+        id="chart"
+        width="auto"
+        @loaded="onChartLoaded"
+      />
+    </StackLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+import { LinearGradient, TileMode } from "@nativescript-community/ui-canvas";
+
+import { LineData } from "@nativescript-community/ui-chart/data/LineData";
+import { LineDataSet } from "@nativescript-community/ui-chart/data/LineDataSet";
+
+import { chartData } from "./chart-data";
+
+export default {
+  computed: {
+    message() {
+      return "Blank {N}-Vue app";
+    },
+  },
+
+  methods: {
+    onChartLoaded() {
+      const chart = this.$refs.chart.nativeView;
+
+      chart.setTouchEnabled(true);
+      chart.setDragEnabled(true);
+      chart.setDoubleTapToZoomEnabled(false);
+      chart.setDrawGridBackground(false);
+      chart.setHighlightPerTapEnabled(true);
+      chart.setHighlightPerDragEnabled(true);
+      chart.setExtraOffsets(8, 10, 8, 8);
+
+      const axisLeft = chart.getAxisLeft();
+      axisLeft.setDrawAxisLine(true);
+      axisLeft.setAxisLineColor("gray");
+      axisLeft.setAxisLineWidth(0.5);
+      axisLeft.setDrawLabels(true);
+      axisLeft.setDrawGridLines(false);
+
+      const axisRight = chart.getAxisRight();
+      axisRight.setDrawAxisLine(false);
+      axisRight.setDrawGridLines(false);
+      axisRight.setDrawLabels(false);
+
+      const xAxis = chart.getXAxis();
+      xAxis.setDrawAxisLine(true);
+      xAxis.setAxisLineColor("gray");
+      xAxis.setAxisLineWidth(0.5);
+      xAxis.setDrawLabels(true);
+      xAxis.setDrawGridLines(false);
+
+      this.setData();
+    },
+
+    setData() {
+      const chart = this.$refs.chart.nativeView;
+      const dataSet = new LineDataSet(
+        chartData.map((v, i) => {
+          return {
+            index: i,
+            y: v[1],
+          };
+        }),
+        "Price Data"
+      );
+
+      dataSet.setLineWidth(2);
+      dataSet.setColor("#E1AFFD");
+      dataSet.setDrawValues(false);
+      dataSet.enableDashedHighlightLine(10, 5, 0);
+      dataSet.setDrawVerticalHighlightIndicator(true);
+      dataSet.setDrawHorizontalHighlightIndicator(false);
+      dataSet.setHighLightColor("#E1AFFD");
+
+      // set gradient
+      setTimeout(() => {
+        dataSet.setDrawFilled(true);
+        dataSet.setFillShader(
+          new LinearGradient(
+            0,
+            0,
+            0,
+            chart.mViewPortHandler.getChartHeight() -
+              chart.mViewPortHandler.offsetBottom(),
+            "#E1AFFD",
+            "#FFFFFF00",
+            TileMode.CLAMP
+          )
+        );
+        chart.invalidate();
+      }, 350);
+
+      const data = new LineData([dataSet]);
+
+      chart.setData(data);
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+@import "@nativescript/theme/scss/variables/blue";
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+// Custom styles
+.fas {
+  @include colorize($color: accent);
+}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+  font-size: 20;
+  horizontal-align: center;
+  vertical-align: center;
+}
 </style>
